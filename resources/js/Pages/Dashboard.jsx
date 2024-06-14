25ffcd27753b582e96ffebb0ff6fd61ccfd6fa72
ganimed <kholyavskij123@gmail.com>
2024-06-14 16:18:59 +0300

    save

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import WallClock from '@/Components/WallClock'; // путь к вашему компоненту WallClock
 
 export default function Dashboard({ auth }) {
     return (
@@ -12,7 +13,14 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-screen-2xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Добрый день, {auth.user.name}!</div>
+                        <div className="p-6 text-gray-900">
+                            Добрый день, {auth.user.name}!
+                        </div>
+                    </div>
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-6">
+                        <div className="p-6 text-gray-900">
+                            <WallClock />
+                        </div>
                     </div>
                 </div>
             </div>
